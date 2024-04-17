cd6fdc584553a9ae20bb7943c147560679e2f7c2
kjg6 <146731014+kjg6@users.noreply.github.com>
2024-04-17 09:41:39 +0200

    a i jeszcze jedna

--- wyniki.php
+++ wyniki.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@500&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="graphics/favicon.png" type="image/x-icon">
 </head>
 <body>
     <style>
