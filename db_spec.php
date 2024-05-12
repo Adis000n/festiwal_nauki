16538027c89a6fd916c9984486e5c25a314e7d72
Adis000n <zapasowyadi@gmail.com>
2024-05-12 13:50:34 +0200

    podłączenie do bazy home pl

--- db_spec.php
+++ db_spec.php
@@ -1,9 +1,15 @@
 <?php
 // Database connection parameters
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "festiwal_nauki";
+//Tu są dane do serwera na home pl
+$host = "46.242.239.199:3380";
+$username = "37911724_festiwal_nauki";
+$password = "xjW1jlazS";
+$database = "37911724_festiwal_nauki";
+//Tu są dane do lokalnie
+// $host = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "festiwal_nauki";
 
 // Create connection
 $con = mysqli_connect($host, $username, $password, $database);
